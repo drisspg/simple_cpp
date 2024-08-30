9271f22f3a15d6ab59d505a8fbe69068ab6de934
drisspg <drisspguessous@gmail.com>
2024-08-29 21:16:54 -0400

    first commit

---
+++ examples/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <print>
+#include "utils.h"
+
+int main() {
+    std::print("Hello, World!\n");
+    std::print("The ceil_div of 10 and 3 is {}\n", s_cpp::ceil_div(10, 3));
+    return 0;
+}
---
+++ src/include/utils.h
@@ -0,0 +1,3 @@
+namespace s_cpp {
+template <typename T> T ceil_div(T a, T b) { return (a + b - 1) / b; }
+} // namespace s_cpp
